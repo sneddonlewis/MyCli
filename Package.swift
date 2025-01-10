29e08354663ea4fbbf982d209e07b68f3dce02ba
Lewis Sneddon <61014463+sneddonlewis@users.noreply.github.com>
2025-01-10 12:12:52 +0000

    new main file with annotation and hello prog with dep

--- Package.swift
+++ Package.swift
@@ -5,10 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "MyCli",
+    dependencies: [
+        .package(url: "https://github.com/apple/example-package-figlet", branch: "main")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "MyCli"),
+            name: "MyCli",
+            dependencies: [
+                .product(name: "Figlet", package: "example-package-figlet")
+            ],
+            path: "Sources")
     ]
 )
